d89b5488c59a3392cbc9b5e326ba2cf511932256
Kawabata Lemon <fresh.mosaic@gmail.com>
2017-10-25 02:05:46 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+
+require('dotenv').config()
+
+const { Qiita } = require('./apis/qiita')
+const { Hatena } = require('./apis/hatena')
+const { Tweet } = require('./apis/tweet')
+
+app.get('/article/:type', function (req, res) {
+
+  if (req.params.type) {
+
+    switch (req.params.type) {
+
+      case 'qiita':
+        Qiita.getArticles(articles => {
+          res.send({instances: articles})
+        })
+      break
+      case 'tweet':
+        Tweet.getArticles(articles => {
+          res.send({instances: articles})
+        })
+        break
+      case 'github':
+        res.send('ぎふはぶの記事を取得します')
+        break
+      case 'blog':
+        Hatena.getArticles(articles => {
+          res.send({instances: articles})
+        })
+        break
+
+      default:
+        res.send('不正なパラメータです')
+      break;
+    }
+    return    
+  } 
+ 
+  res.send('不正なパラメータです')
+});
+
+app.get('/article', (req, res) => {
+  res.send('パラメータを指定してください')
+});
+  
+
+app.listen(3000, () => {});
